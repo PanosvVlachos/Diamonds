39fc13937721d9c1689a4662b0d93dfc7f618eb1
Panagiotis(Panos) Vlachos <panos.vlachos124@hotmail.com>
2025-07-03 11:02:57 -0400

    Add files via upload

--- head(diamonds).r
+++ head(diamonds).r
@@ -1,4 +1,6 @@
-head(diamonds)
+library(ggplot2)
+
+head(diamonds)#install.packages("ggplot2")
 #%%
 a=diamonds
 #%%
@@ -17,9 +19,7 @@ barplot(table(a$color),xlab = "Color", ylab = "Frequency", main = "BAR PLOT OF C
 #%%
 barplot(table(a$clarity),xlab = "Clarity", ylab = "Frequency", main = "BAR PLOT OF CLARITY", col="dodgerblue")
 #%%
-barplot(table(a$cut),xlab = "Cut", ylab = "Frequency", main = "BAR PLOT OF CUT", col="dodgerblue")
-#%%
-ggplot(color, price, outline=FALSE, xlab = "color", ylab = "Price", main = "Color VS Price")
+pie(table(a$cut),xlab = "Cut", ylab = "Frequency", main = "BAR PLOT OF CUT", col="dodgerblue")
 #%%
 ggplot(diamonds, aes(x = cut, y = price/carat)) +
   geom_boxplot(fill = "skyblue") +
@@ -31,4 +31,45 @@ ggplot(diamonds, aes(x = clarity, y = price)) +
   labs(title = "Price per Carat by Clarity",
        y = "Price per Carat")
 #%%
+#CHECKING FOR NORMAL DISTRIBUTION
+x = diamonds$price  # or diamonds$carat, etc.
+hist(x, breaks = 50, probability = TRUE, 
+     col = "lightblue", main = "Histogram with Normal Curve", xlab = "Price")
+lines(density(x), col = "red", lwd = 2)
+
+qqnorm(x)
+qqline(x, col = "red", lwd = 2)
+
+ks.test(x, "pnorm", mean(x), sd(x))
+
+#%%
+# USE MEADIAN- MAD (median absolute deviation) TO USE Z-SCOPE(NORMALIZE IT)
+price <- diamonds$price
+med <- median(price)
+mad_val <- mad(price)
+
+# Modified z-score formula
+mod_z <- 0.6745 * (price - med) / mad_val
+
+# Threshold: usually > 3.5 is considered an outlier
+outliers <- which(abs(mod_z) > 3.5)
+diamonds[outliers, ]
+length(outliers)
+#%%
+# USE MEADIAN- MAD (median absolute deviation) TO USE Z-SCOPE(NORMALIZE IT)
+carat<- diamonds$carat
+med <- median(carat)
+mad_val <- mad(carat)
+
+# Modified z-score formula
+mod_z <- 0.6745 * (carat - med) / mad_val
+
+# Threshold: usually > 3.5 is considered an outlier
+outliers <- which(abs(mod_z) > 3.5)
+diamonds[outliers, ]
+length(outliers)
+#%%
+#CLEANING THE DATA
+diamonds_clean <- diamonds[abs(mod_z) <= 3.5, ]
+dim(diamonds_clean)
 
